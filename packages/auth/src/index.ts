36bcf3dff364e179a18e48e666a2c177d7668324
madtofan <ahmadclab@gmail.com>
2026-04-25 11:51:44 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,40 @@
+import { expo } from "@better-auth/expo";
+import { createDb } from "@mirror-scan/db";
+import * as schema from "@mirror-scan/db/schema/auth";
+import { env } from "@mirror-scan/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [
+      env.CORS_ORIGIN,
+      "mirror-scan://",
+      ...(env.NODE_ENV === "development"
+        ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+        : []),
+    ],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
